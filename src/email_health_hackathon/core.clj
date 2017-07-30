f917e13db3163e1d34c2f99c19d4596b1027bde0
Kenneth Kantzer <ken@Kenneths-MacBook-Pro.local>
2017-07-29 17:56:40 -0700

    calculated chain health length

--- src/email_health_hackathon/core.clj
+++ src/email_health_hackathon/core.clj
@@ -1,7 +1,8 @@
 (ns email-health-hackathon.core
   (:require [clojure.data.csv :as csv]
             [clojure.java.io :as io]
-            [clojure.string :as s])
+            [clojure.string :as s]
+            [email-health-hackathon.stats :as stats])
   (:gen-class))
 
 (defn starting-email? [{subject :message_subject}]
@@ -15,11 +16,54 @@
     (s/starts-with? subject "\"rE:")
     (s/starts-with? subject "\"RE:"))))
 
-(defn parse-chains [content]
-  (doseq [email content]
-    (when (starting-email? email)
-      (println (:message_subject email)))))
+(defn find-starting-emails [content]
+  (persistent!
+    (reduce
+      (fn [acc {:keys [message_subject] :as m}]
+        (assoc! acc message_subject [m]))
+      (transient {})
+      (filter starting-email? content))))
 
+(defn find-original-subject [{subject :message_subject}] ;blows away the "RE:"s in the beginning
+  (s/replace subject #"^\"?([Rr][Ee]: )*" ""))
+
+(defn parse-chains [starting-emails content]
+  (reduce 
+    (fn [acc potential-chain-email]
+      (if (not (starting-email? potential-chain-email))
+        (let [original-subject (find-original-subject potential-chain-email)]
+          (if (contains? acc original-subject)
+            (update acc original-subject #(conj % potential-chain-email))
+            acc))
+        acc))
+    starting-emails
+    content))
+
+(def penultimate
+  (comp second reverse))
+
+; we removed all chains of length 1, and high outliers (likely not one chain) 
+(defn chain-health [chains]
+  (let [chain-counts (mapv #(count (second %)) (seq chains))
+        sorted-chains-without-ones (sort (filterv #(not= 1 %) chain-counts))
+        sorted-chains-without-ones-and-outlier (butlast sorted-chains-without-ones)
+        largest-chain (penultimate (sort-by #(count (second %)) (seq chains)))
+        histogram (reduce
+                    (fn [acc chain]
+                      (let [length (count (second chain))]
+                        (if (contains? acc length)
+                          (update acc length inc)
+                          (assoc acc length 1))))
+                    {}
+                    (seq chains))
+        sorted-histogram (sort-by second (seq histogram))]
+    (float (stats/mean sorted-chains-without-ones-and-outlier))))
+
+; TODO: we are assuming that all emails with the same original subject (taking out RE's) are part of a chain
+; we could improve this by checking timestamps / making sure the recipients are consistent
+; doign this would probably re-allocate some of teh longer "chains" (ex. "Question") to multiple shorter chains
+; so, our numbers for chain length is artificially high.
+; we toss out the highlighters by calculating median.
 (defn -main
   [& args]
   (let [filename "MTSummary_Tenant Message trace report June 5 to June 15_9af49a18-461a-4be9-8402-cafb95611498.csv"]
@@ -27,5 +71,7 @@
       (let [content (doall (csv/read-csv reader))
             first-row (mapv keyword (first content))
             data (rest content)
-            structured-content (mapv #(zipmap first-row %) data)]
-        (parse-chains structured-content)))))
+            structured-content (mapv #(zipmap first-row %) data)
+            starting-emails (find-starting-emails structured-content)
+            chains (parse-chains starting-emails structured-content)]
+        (println (str "Chain Health: " (chain-health chains)))))))
---
+++ src/email_health_hackathon/stats.clj
@@ -0,0 +1,20 @@
+(ns email-health-hackathon.stats
+  (:gen-class))
+
+(defn mean [coll]
+  (let [sum (apply + coll)
+        count (count coll)]
+    (if (pos? count)
+      (/ sum count)
+      0)))
+
+(defn median [coll]
+  (let [sorted (sort coll)
+        cnt (count sorted)
+        halfway (quot cnt 2)]
+    (if (odd? cnt)
+      (nth sorted halfway) ; (1)
+      (let [bottom (dec halfway)
+            bottom-val (nth sorted bottom)
+            top-val (nth sorted halfway)]
+        (mean [bottom-val top-val]))))) ; (2)
